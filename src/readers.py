7661f27ed54d0fcddb25080b52eb770170cb283a
Yasnov Mikhail <mike_yasnov@mail.ru>
2024-01-14 15:22:58 +0300

    Update readers.py

--- src/readers.py
+++ src/readers.py
@@ -1,28 +1,16 @@
-import numpy as np
 import re
 
-
-def read_matrix(
-        input_lines: list, 
-        expected_shape: list
-        ) -> np.array:
+def read_matrix(input_lines: list, expected_shape: list) -> list:
     n, m = expected_shape
     result = []
     for _ in range(m):
-        result.append(list(map(complex, input_lines.pop(0).split())))
-    return np.array(result, dtype=complex).reshape((m, n))
+        result.append([complex(num) for num in input_lines.pop(0).split()])
+    return result
 
+def read_vector(input_lines: list) -> list:
+    return [complex(num) for num in input_lines.pop(0).strip().split()]
 
-def read_vector(
-        input_lines: list
-        ) -> np.array:
-    vector = list(map(complex, input_lines.pop(0).strip().split()))
-    return np.array(vector, dtype=complex).reshape(-1, 1)
-
-
-def parse_input(
-        input_lines: list
-        ) -> (np.array, np.array, np.array):
+def parse_input(input_lines: list) -> (str, dict, dict):
     expression = input_lines.pop(0)
     matrices = {}
     vectors = {}
@@ -38,8 +26,7 @@ def parse_input(
     # Ищем строчные буквы для векторов
     for vector_name in re.findall(r'[a-wyz]', expression):
         if vector_name not in vectors.keys():
-            dim = int(input_lines.pop(0))
+            input_lines.pop(0)  # Previously used to get the dimension of the vector
             vectors[vector_name] = read_vector(input_lines)
             
     return (expression, matrices, vectors)
-
