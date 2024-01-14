e7a6b56234ff6e91236e42602f716f16d746b595
Yasnov Mikhail <mike_yasnov@mail.ru>
2024-01-14 15:22:56 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import numpy as np 
 from src.readers import parse_input
 from src.utils import prepare_equation
-from src.solvers import solve, solve_gaussian
-
+from src.solvers import solve_gaussian
 
 def main(file_name: str) -> str:
     with open(file_name, 'r') as file:
@@ -11,16 +9,12 @@ def main(file_name: str) -> str:
     expression, matrices, vectors = parse_input(input_lines)
     A, b = prepare_equation(expression, matrices, vectors)
     # Решение СЛАУ
-    flag = int(input("Введите способ решения СЛАУ: \n1. Обычный\n2.Гаусс\nВаш вариант ответа: "))
-    if flag == 1:
-        x = solve(A, b)
-    else:
-        b = b.reshape(-1)
-        x = solve_gaussian(np.copy(A), np.copy(b), doPricing = True)
+    x = solve_gaussian(A.copy(), b.copy(), doPricing=True)
     if isinstance(x, str):
         return x
     else:
         return '\n'.join(f'x{i+1} = {elem}' for i, elem in enumerate(x))
+
 
 
 if __name__ == "__main__":
