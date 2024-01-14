b24b348289736d441f68e3dbf7a1f691f65c3002
Yasnov Mikhail <mike_yasnov@mail.ru>
2024-01-14 15:23:00 +0300

    Update solvers.py

--- src/solvers.py
+++ src/solvers.py
@@ -1,45 +1,27 @@
-import numpy as np
-from numpy.linalg import LinAlgError
-
-def solve(
-        A: np.array, 
-        b: np.array
-        ) -> np.array:
-    try:
-        return np.linalg.solve(A, b)
-    except LinAlgError as e:
-        if 'Singular matrix' in str(e):
-            return "нет решений"
-        else:
-            return "бесконечное количество решений"
-        
-
-def solve_gaussian(
-        A: np.array, 
-        b: np.array, 
-        doPricing: bool = True
-        ) -> np.array:
-    
+def solve_gaussian(A: list, b: list, doPricing: bool = True) -> list:
     n = len(A)
-    if b.size != n:
-        raise ValueError("Неверный ввод. "+ "A & b.", b.size, n)
-    
-    for k in range(n-1):
+    if len(b) != n:
+        raise ValueError("Неверный ввод. A & b.", len(b), n)
+
+    for k in range(n - 1):
         if doPricing:
-            maxindex = abs(A[k:,k]).argmax() + k
-            if A[maxindex, k] == 0:
+            maxindex = k + max(range(k, n), key=lambda x: abs(A[x][k]))
+            if A[maxindex][k] == 0:
                 raise ValueError("Матрица является единственной.")
             if maxindex != k:
-                A[[k,maxindex]] = A[[maxindex, k]]
-                b[[k,maxindex]] = b[[maxindex, k]]
+                A[k], A[maxindex] = A[maxindex], A[k]
+                b[k], b[maxindex] = b[maxindex], b[k]
         else:
-            if A[k, k] == 0:
+            if A[k][k] == 0:
                 raise ValueError("Поворотный элемент равен нулю. Попробуйте установить для doPricing значение True.")
-        for row in range(k+1, n):
-           multiplier = A[row,k]/A[k,k]
-           A[row, k:] = A[row, k:] - multiplier*A[k, k:]
-           b[row] = b[row] - multiplier*b[k]
-    x = np.zeros(n, dtype=complex)
-    for k in range(n-1, -1, -1):
-        x[k] = (b[k] - np.dot(A[k,k+1:],x[k+1:]))/A[k,k]
+
+        for row in range(k + 1, n):
+            multiplier = A[row][k] / A[k][k]
+            A[row][k:] = [ar - multiplier * ak for ar, ak in zip(A[row][k:], A[k][k:])]
+            b[row] = b[row] - multiplier * b[k]
+
+    x = [0] * n
+    for k in range(n - 1, -1, -1):
+        x[k] = (b[k] - sum(A[k][j] * x[j] for j in range(k + 1, n))) / A[k][k]
+
     return x
